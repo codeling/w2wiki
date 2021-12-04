2234a9181832f99d28adf75088195899080bb17b
Mejans <61360811+Mejans@users.noreply.github.com>
2021-12-04 13:20:42 +0100

    Creation of Occitan locale file

---
+++ locales/oc.php
@@ -0,0 +1,32 @@
+<?php if (!defined('W2APP')){ die('No direct access.'); }
+
+/**
+ * Locale set
+ *
+ * Do not change variable name $w2_word_set.
+ * Rewrite values in your locale and rename this file to locale code.
+ * Set "LOCALE" to your locale in config.php.
+ */
+$w2_word_set = array(
+	// Buttons / Labels
+	'Home'   => 'Acuèlh',
+	'New'    => 'Nòu',
+	'Edit'   => 'Editar',
+	'Upload' => 'Enviar',
+	'All'    => 'Tot',
+	'Recent' => 'Recents',
+	'Exit'   => 'Sortir',
+	'Search' => 'Recercar',
+	'Save'   => 'Salvar',
+	'Cancel' => 'Anullar',
+	'Saved'  => 'Salvat',
+	'Log In' => 'Connexion',
+	'Title'  => 'Títol',
+	// Messages
+	'Upload error' => 'Error de mandadís',
+	'Upload error: invalid file type' => 'Error de mandadís : tipe de fichièr invalid',
+	'Image uploading has been disabled on this installation.' => 'Mandadís d’imatge desactivat sus aquesta installacion.',
+	// Override TITLE_DATE and TITLE_DATE_NO_TIME if set.
+	'date_format'         => 'Y/m/d %Hh%M',
+	'date_format_no_time' => 'Y/m/d',
+);
