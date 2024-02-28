06c06dfcea11aa79485807c680a882dd2182f35d
Bernhard Froehler <bernhard@bfroehler.info>
2024-02-28 15:39:59 +0100

    prevent inadvertent navigation away from edited content; spaces -> tabs in .js

--- wiki.js
+++ wiki.js
@@ -1,45 +1,64 @@
 
 function toggleDrawer()
 {
-     document.getElementById('drawer').classList.toggle('inactive');
+	document.getElementById('drawer').classList.toggle('inactive');
 }
 
 // based on https://www.w3schools.com/howto/howto_js_draggable.asp
 function makeElementDraggable(elmnt)
 {
-    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    elmnt.onmousedown = dragMouseDown;
-  
-    function dragMouseDown(e)
-    {
-        e = e || window.event;
-        e.preventDefault();
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        document.onmouseup = closeDragElement;
-        document.onmousemove = elementDrag;
-    }
-  
-    function elementDrag(e)
-    {
-        e = e || window.event;
-        e.preventDefault();
-        pos1 = pos3 - e.clientX;
-        pos2 = pos4 - e.clientY;
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-    }
-  
-    function closeDragElement()
-    {
-       document.onmouseup = null;
-       document.onmousemove = null;
-    }
-}
+	var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+	elmnt.onmousedown = dragMouseDown;
 
-window.onload = function(e)
+	function dragMouseDown(e)
+	{
+		e = e || window.event;
+		e.preventDefault();
+		pos3 = e.clientX;
+		pos4 = e.clientY;
+		document.onmouseup = closeDragElement;
+		document.onmousemove = elementDrag;
+	}
+
+	function elementDrag(e)
+	{
+		e = e || window.event;
+		e.preventDefault();
+		pos1 = pos3 - e.clientX;
+		pos2 = pos4 - e.clientY;
+		pos3 = e.clientX;
+		pos4 = e.clientY;
+		elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+		elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+	}
+
+	function closeDragElement()
+	{
+		document.onmouseup = null;
+		document.onmousemove = null;
+	}
+}
+document.addEventListener('DOMContentLoaded', () =>
 {
-    makeElementDraggable(document.getElementById("drawer"));
-};
+	makeElementDraggable(document.getElementById("drawer"));
+
+	// prevent inadvertent navigation away from edited content:
+	modified = false;
+	addEventListener('beforeunload', (event) =>
+	{
+		if (modified)
+		{
+			event.preventDefault();
+		}
+	});
+	let textArea = document.getElementById("text");
+	textArea.addEventListener('input', () =>
+	{
+		modified = true;
+	});
+	let saveBtn = document.getElementById("save");
+	saveBtn.addEventListener('click', () =>
+	{
+		modified = false;
+	});
+});
